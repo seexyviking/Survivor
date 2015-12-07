f158e3adb1d981030ac830fb0d2b7866428b3013
mario.cherchi_tmp <mario.cherchi_tmp@CNAM-APPERT-61.cnam-ca.priv>
2015-12-07 14:32:17 +0100

    Implémentation de la classe Joueur (package Jeu_Carte).

--- src/Jeu_Cartes/Joueur.java
+++ src/Jeu_Cartes/Joueur.java
@@ -1,8 +1,25 @@
 package Jeu_Cartes;
 
 public class Joueur {
-
-	private String nom ;
 	
+	private String nom ;
+	private ArrayList<Carte> main ;
+	
+	public Joueur(String nom){
+		this.nom = nom ;
+		this.main = new ArrayList<Carte>() ;
+	}
+	
+	public String getNom(){
+		return this.nom ;
+	}
+	
+	public void ajouterCarte(Carte c){
+		this.main.add(c);
+	}
+	
+	public void supprimerCarte(int position){
+		this.main.detete(position);
+	}
 	
 }
